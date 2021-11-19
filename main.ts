74cfa15fe0d665d84d555f438fde75c41c820bb4
isataicl <93624536+isataicl@users.noreply.github.com>
2021-11-19 11:16:31 -0700

    408

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Math.round(gatorEnvironment.getMeasurement(measurementType.degreesF)))
+    if (75 < gatorEnvironment.getMeasurement(measurementType.degreesF)) {
+        basic.showIcon(IconNames.Sad)
+    } else if (gatorEnvironment.getMeasurement(measurementType.degreesF) < 65) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
+input.onButtonPressed(Button.AB, function () {
+    let x = 0
+    basic.showNumber(Math.round(gatorSoil.moisture(AnalogPin.P0, GatorSoilType.Moisture, DigitalPin.P1)))
+    if (0.3 > x) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(gatorEnvironment.getMeasurement(measurementType.eCO2))
+    strip = neopixel.create(DigitalPin.P12, 24, NeoPixelMode.RGB)
+    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+    basic.pause(5000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+})
+let strip: neopixel.Strip = null
+gatorEnvironment.beginEnvironment()
